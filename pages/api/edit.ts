d6da75ea0b2fcdc5f6b31276b6fa314a67843ccf
Moyez <moyezrabbani@gmail.com>
2023-05-06 13:56:00 +0530

    Fixed Prisma Imports

--- pages/api/edit.ts
+++ pages/api/edit.ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import serverAuth from "../../libs/serverAuth";
+import prisma from "../../libs/prisma-db"
 export default async function handler (req: NextApiRequest, res: NextApiResponse) { 
 
     if(req.method !== 'PATCH') {
--- pages/api/users/[userId].ts
+++ pages/api/users/[userId].ts
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
+import prisma from "../../../libs/prisma-db"
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if(req.method !== 'GET') {
